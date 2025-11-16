3eacc8d9f6cede0fd3eb149456d1c49f903d7075
thakurudit-dev <udit.ntzl@gmail.com>
2025-11-16 16:53:33 +0530

    check confiig user and email

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(bodyParser.json());
 app.get('/',  function (req, res) {
     res.send('Welcome to our project 2');
 });
-
+console.loh("check confiig user and email")
 // Listening to Localhost Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
